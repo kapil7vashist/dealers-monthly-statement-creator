d466ff708d3116059b1355213ae4df88a41607a1
Kapil Vashist <kapil@aegiscovenant.com>
2024-09-09 16:58:40 +0530

    updated folder id for august 2024 reports;

--- constants/constants.js
+++ constants/constants.js
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 const month = dayjs(new Date).subtract(1, 'month').format('MMMM');
 const year = dayjs(new Date).year();
 const currentMonthSheet = `${month} ${year}`;
-const folderId = '1RcRr7RVrGwO4JIBXbGunsHB1zLrYG9b4';
+const folderId = '1QaXzoQWab-G3lHu-Jp-9q3lz3VZYip3f';
 const spreadsheetId = '1y5KC9CK9KcOSzp2hBSN66VGMlNnRA3f6ya71qX0aouM';
 
 export {
